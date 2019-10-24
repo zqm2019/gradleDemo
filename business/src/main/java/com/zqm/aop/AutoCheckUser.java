05ed9bd4609358336538fff1f520eda521b75396
zhaqianming <zhaqianming@tuniu.com>
2019-10-24 13:32:22 +0800

    切面编程&添加注释

--- business/src/main/java/com/zqm/aop/AutoCheckUser.java
+++ business/src/main/java/com/zqm/aop/AutoCheckUser.java
@@ -5,18 +5,64 @@ package com.zqm.aop;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * TODO: description
- * Date: 2019-10-23
+ * 元注解: 注解的注解，只作用在注解身上
  *
  * @author zhaqianming
+ * @Retention 应用到一个注解上的时候，它解释说明了这个注解的的存活时间
+ *      SOURCE参数只在源码阶段保留，在编译器进行编译时它将被丢弃忽视
+ *      CLASS注解只被保留到编译进行的时候，它并不会被加载到 JVM 中。
+ *      RUNTIME(最久) 注解可以保留到程序运行的时候，它会被加载进入到 JVM 中，所以在程序运行时可以获取到它们
+ *
+ * @Target 指定了注解运用的地方。
+ *      ElementType.ANNOTATION_TYPE 可以给一个注解进行注解
+ *      ElementType.CONSTRUCTOR 可以给构造方法进行注解
+ *      ElementType.FIELD 可以给属性进行注解
+ *      ElementType.LOCAL_VARIABLE 可以给局部变量进行注解
+ *      ElementType.METHOD 可以给方法进行注解
+ *      ElementType.PACKAGE 可以给一个包进行注解
+ *      ElementType.PARAMETER 可以给一个方法内的参数进行注解
+ *      ElementType.TYPE 可以给一个类型进行注解，比如类、接口、枚举
+ *
+ * @Documented  它的作用是能够将注解中的元素包含到 Javadoc 中去。
+ *
+ * @Inherited  如果一个超类被 @Inherited 注解过的注解进行注解的话，
+ *             那么如果它的子类没有被任何注解应用的话，那么这个子类就继承了超类的注解。
+ *
+ * @Repeatable Repeatable 是 Java 1.8 才加进来的，所以算是一个新的特性。
+ *             通常是注解的值可以同时取多个。
+ *
+ * Date: 2019-10-23
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 @Documented
 public @interface AutoCheckUser {
 }
+
+/*
+@interface Persons {
+    Person[]  value();
+}
+
+
+@Repeatable(Persons.class)
+@interface Person {
+    String role
+    default "";
+}
+
+
+@Person(role = "artist")
+@Person(role = "coder")
+@Person(role = "PM")
+public class SuperMan {
+}
+*/
+
+
--- business/src/main/java/com/zqm/aop/UserIdCheckAop.java
+++ business/src/main/java/com/zqm/aop/UserIdCheckAop.java
@@ -25,27 +25,40 @@ import com.alibaba.fastjson.JSON;
  *
  * @author zhaqianming
  */
-@Aspect
-@Component
-@Order(1)
+@Aspect //定义一个切面
+@Component // 把普通pojo实例化到spring容器中
+@Order(1) //多个注解是优先级最高，值越小优先级越高
 public class UserIdCheckAop {
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
     /**
-     * 定义切点
+     * 定义切点 切点也可以直接写在通知的value层
      */
     @Pointcut(value = "@annotation(com.zqm.aop.AutoCheckUser)")
     public void cut() {
 
     }
  private static final List list = Arrays.asList("123","345","789");
+
+
+    /*
+    可自定义切点位置，针对不同切点，方法上的@Around()可以这样写ex：
+    @Around(value = "methodPointcut() && args(..)")
+    @Pointcut(value = "@annotation(com.rq.aop.common.annotation.MyAnnotation)")
+    public void methodPointcut(){}
+
+
+    @Pointcut(value = "@annotation(com.rq.aop.common.annotation.MyAnnotation2)") //在所有标记@MyAnnotation2方法上切入
+    public void methodPointcut2(){}
+    */
+
     /**
      * 拦截器实现
      */
 
     @Around("cut()")
     public Object FilterUser(ProceedingJoinPoint joinPoint) {
-        Object result = null;
+        Object result;
         Object[] args = joinPoint.getArgs();
         try {
             //访问目标方法的参数：
@@ -55,7 +68,7 @@ public class UserIdCheckAop {
                     return "未登录";
                 }
                 if (!list.contains(userId)){
-                    return "为授权用户";
+                    return "未授权用户";
                 }
             }
 
@@ -69,5 +82,35 @@ public class UserIdCheckAop {
         return result;
 
     }
+
+    /**
+     * //execution（）                表达式的主体；
+     * 第一个”*“符号                   表示返回值的类型任意；
+     * com.zqm.service.impl           AOP所切的服务的包名，即，我们的业务部分
+     *  包名后面的”..“                  表示当前包及子包
+     *  第二个”*“                      表示类名，*即所有类。此处可以自定义
+     *  .*(..)                        表示任何方法名，括号表示参数，两个点表示任何参数类型
+     * @param point
+     * @return
+     * @throws Throwable
+     */
+
+    //只定义切面
+    @Around("execution(* com.zqm.service.impl..*.*(..))")
+    public Object process(ProceedingJoinPoint point) throws Throwable {
+        System.out.println("@Around：执行目标方法之前...");
+        //访问目标方法的参数：
+        Object[] args = point.getArgs();
+        if (args != null && args.length > 0 && args[0].getClass() == String.class) {
+            args[0] = "改变后的参数1";
+        }
+        //用改变后的参数执行目标方法
+        Object returnValue = point.proceed(args);
+        System.out.println("@Around：执行目标方法之后...");
+        System.out.println("@Around：被织入的目标对象为：" + point.getTarget());
+        return "原返回值：" + returnValue + "，这是返回结果的后缀";
+    }
+
+
 
 }
