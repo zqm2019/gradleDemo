f31712960d24282ecacdd5f86d418e3ad8482505
zhaqianming <60546@alibaba-inc.com>
2022-01-21 10:27:26 +0800

    socket

---
+++ src/main/java/com/zqm/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package com.zqm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @describe:
+ * @author:zqm
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpoint() {
+        return new ServerEndpointExporter();
+    }
+}
